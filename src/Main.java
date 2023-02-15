93a4d43c8fdb348a8f7c90aa06666d81fbe12ff0
повелитель <karakozova44d@mail.ru>
2023-02-15 09:21:08 +0300

    added calculation of bonus miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 11111;
+        int bonus = amount / 20;
+
+        System.out.println("Стоимость билета:" + amount);
+        System.out.println("Начисленно бонусных миль:" + bonus);
+
